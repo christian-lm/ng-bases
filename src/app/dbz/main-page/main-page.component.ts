68d1153ee764866156b6c7449fd2bf8e3162b883
Christian Lozano <christian.lozano@neoris.com>
2022-10-10 15:56:16 +0200

    - Se inicializará el array en los servicios - Se crea un getter para el listado de personajes en el servicio - Se cambian las referencias para que lo obtenga del servicio

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {Personaje} from "../interfaces/dbz.interface";
-import {DbzService} from "../services/dbz.service";
 
 @Component({
   selector: 'app-main-page',
@@ -8,26 +7,11 @@ import {DbzService} from "../services/dbz.service";
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegetta',
-      poder: 8500
-    },
-  ];
-
   nuevo: Personaje = {
     nombre: '',
     poder: 0
   }
 
-  agregarNuevoPersonaje(argumento: Personaje) {
-    this.personajes.push(argumento);
-  }
-
-  constructor(private dbzService: DbzService) {
+  constructor() {
   }
 }
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,8 +1,24 @@
 import {Injectable} from "@angular/core";
+import {Personaje} from "../interfaces/dbz.interface";
 
 // Decorador de un servicio
 @Injectable()
 export class DbzService {
+
+  private _personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegetta',
+      poder: 8500
+    },
+  ];
+
+  get personajes(): Personaje[] {
+    return [...this._personajes]; // Los ... y el get en un campo privado rompe la referencia de JS
+  }
 
   constructor() {
     console.log('Servicio inicializado');
