c0a2979fcbe323c571a51cc8f0efe587de1b5c41
Christian Lozano <christian.lozano@neoris.com>
2022-10-10 13:24:15 +0200

    - Se crea el componente hijo de personajes y se usa dentro de main-page

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html'
+})
+export class PersonajesComponent {
+  personajes: any[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegetta',
+      poder: 8500
+    },
+  ];
+}
