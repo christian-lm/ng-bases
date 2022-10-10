fad458dcbfb30e83bdf4d3abcddd210aab08b78a
Christian Lozano <christian.lozano@neoris.com>
2022-10-10 11:23:11 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ContadorComponent} from "./contador/contador.component";
+import {HeroeComponent} from "./heroes/heroe/heroe.component";
+import {ListadoComponent} from './heroes/listado/listado.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // Para colocar componentes
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent,
+    ListadoComponent
   ],
-  imports: [
+  imports: [ // Para colocar modulos
     BrowserModule
   ],
-  providers: [],
+  providers: [], // Para colocar servicios
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
