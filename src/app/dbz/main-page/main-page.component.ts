a6297bdaf83a0400f9d1b78d412239fb8227de08
Christian Lozano <christian.lozano@neoris.com>
2022-10-10 13:04:23 +0200

    Se añade la obtencion de los campos de formulario con angular

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,28 @@
 import {Component} from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
   agregar() {
     //$event.preventDefault(); // Evita el refresh del formulario || Con ngSubmit no es necesario
 
-    console.log('Esta es una prueba');
+    console.log(this.nuevo);
+  }
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
   }
 }
